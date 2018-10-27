4ee1ec2ad6c2613563a338c3f028521239c8525c
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2018-10-26 17:56:07 -0700

    updates

--- active/main.go
+++ active/main.go
@@ -17,6 +17,7 @@ type data struct {
 }
 
 const dateFormat = "2006-01-02"
+const dateFormatFull = "2006-01-02 15:04:05"
 
 func main() {
 	targetDateFlag := flag.String("targetDate", "", "Target date")
@@ -29,49 +30,50 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	targetEnd := targetDate.Add(24 * 90 * time.Hour)
+	earliestIssuance := targetDate.Add(-24 * 90 * time.Hour)
 
-	var err error
-	reader := csv.NewReader(os.Stdin)
-	reader.Comma = '\t'
 	ch := make(chan data, 100000)
-	serialCount := make(map[string]map[string]struct{})
+	serialCount := make(map[string]struct{})
+
 	go func() {
 		for d := range ch {
-			date, err := time.Parse(dateFormat, *d.date)
+			date, err := time.Parse(dateFormatFull, d.date)
 			if err != nil {
 				log.Fatal(err)
 			}
-			if date.Before(targetDate) || date.After(targetEnd) {
+			if !(date.After(earliestIssuance) && date.Before(targetDate)) {
 				continue
 			}
-			if serialCount[d.date] == nil {
-				serialCount[d.date] = make(map[string]struct{})
-			}
-			serialCount[d.date][string(d.serialBytes[6:])] = struct{}{}
+			serialCount[string(d.serialBytes[6:])] = struct{}{}
 		}
 
-		for k, v := range serialCount {
-			fmt.Printf("%s\t%d\n", k, len(v))
-		}
+		fmt.Printf("%s\t%d\n", *targetDateFlag, len(serialCount))
 		os.Exit(0)
 	}()
 
-	for {
-		record, err := reader.Read()
-		if err != nil {
-			break
-		}
-		_, date, serial := record[1], record[2], record[3]
-		date = date[:10]
-		serialBytes, err := hex.DecodeString(serial)
+	for _, filename := range flag.Args()[1:] {
+		f, err := os.Open(filename)
 		if err != nil {
 			log.Fatal(err)
 		}
-		ch <- data{date: date, serialBytes: serialBytes}
-	}
-	if err != nil && err != io.EOF {
-		log.Fatal(err)
+		reader := csv.NewReader(f)
+		reader.Comma = '\t'
+
+		for {
+			record, err := reader.Read()
+			if err != nil {
+				break
+			}
+			_, date, serial := record[1], record[2], record[3]
+			serialBytes, err := hex.DecodeString(serial)
+			if err != nil {
+				log.Fatal(err)
+			}
+			ch <- data{date: date, serialBytes: serialBytes}
+		}
+		if err != nil && err != io.EOF {
+			log.Fatal(err)
+		}
 	}
 	close(ch)
 	select {}
