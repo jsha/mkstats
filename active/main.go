a7329ead6f7e3806623541b2435d654b2e7f8f69
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2018-10-30 16:58:51 -0700

    Refactor a bit and add all three types.

--- active/main.go
+++ active/main.go
@@ -8,16 +8,79 @@ import (
 	"io"
 	"log"
 	"os"
+	"runtime"
+	"strings"
 	"time"
+
+	"golang.org/x/net/publicsuffix"
 )
 
 type data struct {
-	date        string
-	serialBytes []byte
+	date         string
+	serialBytes  []byte
+	reversedName string
 }
 
 const dateFormat = "2006-01-02"
 const dateFormatFull = "2006-01-02 15:04:05"
+
+func ReverseName(domain string) string {
+	labels := strings.Split(domain, ".")
+	for i, j := 0, len(labels)-1; i < j; i, j = i+1, j-1 {
+		labels[i], labels[j] = labels[j], labels[i]
+	}
+	return strings.Join(labels, ".")
+}
+
+// PrintMemUsage outputs the current, total and OS memory being used. As well as the number
+// of garage collection cycles completed.
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	bToGB := func(b uint64) uint64 {
+		return b / 1024 / 1024
+	}
+
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v GB", bToGB(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v GB", bToGB(m.TotalAlloc))
+	fmt.Printf("\tSys = %v GB", bToGB(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func process(ch chan data, targetDate time.Time) {
+	earliestIssuance := targetDate.Add(-24 * 90 * time.Hour)
+	serialCount := make(map[string]struct{})
+	reversedNameCount := make(map[string]struct{})
+	registeredDomainCount := make(map[string]struct{})
+	for d := range ch {
+		date, err := time.Parse(dateFormatFull, d.date)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if !(date.After(earliestIssuance) && date.Before(targetDate)) {
+			continue
+		}
+		serialCount[string(d.serialBytes[6:])] = struct{}{}
+		reversedNameCount[d.reversedName] = struct{}{}
+		forwardName := ReverseName(d.reversedName)
+		eTLDPlusOne, err := publicsuffix.EffectiveTLDPlusOne(forwardName)
+		// EffectiveTLDPlusOne errors when its input is exactly equal to a public
+		// suffix, which sometimes happens. In that case, just count the name
+		// itself.
+		if err != nil {
+			eTLDPlusOne = forwardName
+		}
+		registeredDomainCount[eTLDPlusOne] = struct{}{}
+	}
+
+	targetDateFormatted := targetDate.Format(dateFormat)
+	fmt.Printf("serials %s\t%d\n", targetDateFormatted, len(serialCount))
+	fmt.Printf("names %s\t%d\n", targetDateFormatted, len(reversedNameCount))
+	fmt.Printf("registered %s\t%d\n", targetDateFormatted, len(registeredDomainCount))
+	PrintMemUsage()
+	os.Exit(0)
+}
 
 func main() {
 	targetDateFlag := flag.String("targetDate", "", "Target date")
@@ -30,27 +93,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	earliestIssuance := targetDate.Add(-24 * 90 * time.Hour)
 
 	ch := make(chan data, 100000)
-	serialCount := make(map[string]struct{})
 
-	go func() {
-		for d := range ch {
-			date, err := time.Parse(dateFormatFull, d.date)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if !(date.After(earliestIssuance) && date.Before(targetDate)) {
-				continue
-			}
-			serialCount[string(d.serialBytes[6:])] = struct{}{}
-		}
+	go process(ch, targetDate)
 
-		fmt.Printf("%s\t%d\n", *targetDateFlag, len(serialCount))
-		os.Exit(0)
-	}()
+	read(ch, flag.Args()[1:])
+}
 
+func read(ch chan data, filenames []string) {
 	for _, filename := range flag.Args()[1:] {
 		f, err := os.Open(filename)
 		if err != nil {
@@ -64,12 +115,12 @@ func main() {
 			if err != nil {
 				break
 			}
-			_, date, serial := record[1], record[2], record[3]
+			reversedName, date, serial := record[1], record[2], record[3]
 			serialBytes, err := hex.DecodeString(serial)
 			if err != nil {
 				log.Fatal(err)
 			}
-			ch <- data{date: date, serialBytes: serialBytes}
+			ch <- data{date: date, serialBytes: serialBytes, reversedName: reversedName}
 		}
 		if err != nil && err != io.EOF {
 			log.Fatal(err)
